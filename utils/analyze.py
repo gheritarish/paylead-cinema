f167a6c399eec48d67d86750c4716564f09a7fe8
Gheritarish <ilissiae@gmail.com>
2021-10-03 15:21:30 +0200

    :memo:  Add docstring to the different functions

--- utils/analyze.py
+++ utils/analyze.py
@@ -1,11 +1,19 @@
-from typing import List
-
 import geopandas as gpd
 import pandas as pd
 from loguru import logger
 
 
 def cinemas_in_town(data: gpd.GeoDataFrame, town_name: str) -> int:
+    """Function to find the number of theaters in a town, given the town name.
+
+    Args:
+        data (gpd.GeoDataFrame): A GeoDataFrame with the theaters data.
+        town_name (str): The name of the town of which we want the number of theaters.
+
+    Returns:
+        (int): The number of theaters in the given town.
+
+    """
     data = data["com_nom"].value_counts()
     try:
         theaters_town = data[town_name]
@@ -16,6 +24,20 @@ def cinemas_in_town(data: gpd.GeoDataFrame, town_name: str) -> int:
 
 
 def biggest_networks(data: gpd.GeoDataFrame, number_of_networks: int) -> pd.Series:
+    """Function to get the biggest networks, by number of theaters.
+
+    This function will limit itself to the number given by the user.
+
+    Args:
+        data (gpd.GeoDataFrame): A GeoDataFrame with the theaters data.
+        number_of_networks (int): The number of biggest networks we want to get.
+
+    Returns:
+        (pd.Series) A series with the names of the network and their number of theaters.
+
+    Raises:
+        ValueError: This exception is raised if the number_of_network given is negative.
+    """
     data = data["marque"].value_counts()
     if number_of_networks > 0:
         biggest_networks_numbers = data[:number_of_networks]
@@ -26,6 +48,25 @@ def biggest_networks(data: gpd.GeoDataFrame, number_of_networks: int) -> pd.Seri
 
 
 def analyze_network(data: gpd.GeoDataFrame, network_name: str):
+    """Function to analyze a given network of theaters.
+
+    This function gives the theater with the highest number of screens,
+    the theater with the highest number of seats,
+    and the number of theaters with available 3D projections.
+
+    Args:
+        data (gpd.GeoDataFrame): A GeoDataFrame with the theaters data.
+        network_name (str): The name of the network to analyze.
+
+    Returns:
+        (List): A list with, in order: the name of the theater with the highest number
+        of screens, and its number of screens; the name of the theater with the highest
+        number of seats, and its number of seats; the number of theaters with available
+        3D projection in the network.
+
+    Raises:
+        ValueError: This exception is raised if the network_name is unknown.
+    """
     data = data[data["marque"] == network_name]
     if len(data) > 0:
         max_screens = data["nb_screens"][data["nb_screens"].notnull()].astype(int).max()
--- utils/parse_dataset.py
+++ utils/parse_dataset.py
@@ -5,7 +5,18 @@ import geopandas as gpd
 from loguru import logger
 
 
-def find_geojson_file_in_zip(zip_file: str) -> str:
+def find_geojson_file_in_zip(zip_file: str) -> ZipInfo:
+    """Function to find the geojson file in a given zip file.
+
+    Args:
+        zip_file (str): The path to the zip file.
+
+    Returns:
+        (ZipInfo) The geojson file in the archive, as a ZipInfo object.
+
+    Raises:
+        Exception: This exception is raised if there are several files corresponding.
+    """
     with ZipFile(zip_file, "r") as file_zip:
         geojson_file = re.compile(r"^[\w-]+\.geojson$")
         # zip_members = file_zip.namelist()
@@ -16,18 +27,35 @@ def find_geojson_file_in_zip(zip_file: str) -> str:
     if len(geojson_files) == 1:
         return geojson_files[0]
     else:
-        logger.warning(
-            f"Several files have a corresponding name: {len(geojson_files)} files."
-        )
+        raise Exception("Several files correspond to the given regex.")
 
 
 def extract_geojson_file(geojson_file: ZipInfo, zip_file: str) -> str:
+    """Function to extract a geojson file from a zip archive.
+
+    Args:
+        geojson_file (ZipInfo): The geojson file to extract, as a ZipInfo object.
+        zip_file (str): The path to the zip file.
+
+    Returns:
+        (str): The path to the extracted file.
+
+    """
     with ZipFile(zip_file, "r") as file_zip:
         file_zip.extract(member=geojson_file)
         return geojson_file.filename
 
 
 def parse_dataset(geojson_path: str) -> gpd.GeoDataFrame:
+    """Function to create a GeoDataFrame from a given geojson file.
+
+    Args:
+        geojson_path (str): The path to the geojson file.
+
+    Returns:
+        (gpd.GeoDataFrame) The GeoDataFrame constructed by reading the geojson file.
+
+    """
     try:
         cinema_data = gpd.read_file(geojson_path)
         return cinema_data
