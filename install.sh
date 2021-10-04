c38abe7f2860c3163187888cf75b95c3378d1438
Gheritarish <ilissiae@gmail.com>
2021-10-04 14:30:32 +0200

    :bug:  Fix install script by changing "source" to "."

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 echo "Creating environment"
 python3.8 -m venv paylead
-source ./paylead/bin/activate
+. ./paylead/bin/activate
 
 echo "Installing dependencies..."
 pip install -r ./requirements.txt
